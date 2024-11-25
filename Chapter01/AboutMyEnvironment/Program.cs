9ac21e66b2e121ba52c0785d9515ac6523bd9fc8
Saeid Ghasemi <sagharmax@outlook.com>
2024-11-25 09:59:28 +0100

    feat: Initial code to work with CSharp.

--- Chapter01/AboutMyEnvironment/Program.cs
+++ Chapter01/AboutMyEnvironment/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(Environment.CurrentDirectory);
+Console.WriteLine(Environment.OSVersion);
+Console.WriteLine($"Namespace: {typeof(Program).Namespace ?? "<null>"}");
--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, C#!");
+string name = typeof(Program).Namespace ?? "<null>";
+Console.WriteLine($"Namespace: {name}");
+throw new Exception();

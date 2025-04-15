e91635359acb22a1b18233a17caa6004fb7bd9a8
Saeid Ghasemi <sagharmax@outlook.com>
2025-04-15 09:32:55 +0200

    feat: implement functionality to reveal C# vocabulary extent

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-#error version
+﻿using System.Reflection;
+//#error version
+
+
+///  Revealing the extent of the C# vocabulary
+Assembly? myApplication = Assembly.GetEntryAssembly();
+if (myApplication is null)
+{
+    Console.WriteLine("No assembly found.");
+    return;
+}
+
+foreach (AssemblyName name in myApplication.GetReferencedAssemblies())
+{
+    Assembly assembly = Assembly.Load(name);
+    int methodCount = 0;
+    foreach (TypeInfo type in assembly.DefinedTypes)
+    {
+        methodCount += type.GetMethods().Length;
+    }
+    Console.WriteLine($"{assembly.DefinedTypes.Count()} types with {methodCount:N0} methods in {name.Name} assembly.");
+}

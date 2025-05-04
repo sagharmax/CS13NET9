8ecb589fc1bd4a0db98ba675f2395eb69209f591
Saeid Ghasemi <sagharmax@outlook.com>
2025-05-04 17:50:08 +0200

    ✨ feat: Add AsyncConsole project with initial implementation for HTTP request

---
+++ Chapter02/AsyncConsole/Program.cs
@@ -0,0 +1,3 @@
+﻿HttpClient client = new();
+HttpResponseMessage response = await client.GetAsync("https://www.microsoft.com/");
+Console.WriteLine($"Microsoft's homepage has {response.Content.Headers.ContentLength} bytes.");
